2c6d256416c7d6a89d2cde6e730c9c6a01453195
Ghost44511 <idrissoukarim102@gmail.com>
2026-05-10 15:58:57 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import connectToWhatsapp from './Hub/shadow.js'
-import handleIncomingMessage from './events/messageHandler.js'
-
-(async() => {
-    await connectToWhatsapp(handleIncomingMessage)
-        console.log('established !')
-})()
